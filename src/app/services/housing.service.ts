2b8fa3a7822d2bff3c93e06990173ed8ca844b31
kabil <kabil@ancbusiness.com>
2021-05-31 17:55:03 +0530

    commit

--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import {map} from "rxjs/operators"
 @Injectable({
   providedIn: 'root'
 })
@@ -9,5 +10,7 @@ export class HousingService {
   getAllproperties()
   {
     return this.http.get('data/properties.json');
+     
+  
   }
 }
