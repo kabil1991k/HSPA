8fa54b827a22365667c104600fe58a9e71e0b0d2
kabil <kabil@ancbusiness.com>
2021-05-31 14:16:14 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import  {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import {PropertyCardComponent} from './property/property-card/property-card.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    PropertyListComponent,
+    PropertyCardComponent,
+      NavBarComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/property/property-list/property-list.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+
+
+@Component({
+  selector:'app-property-list',
+  templateUrl:'property-list.component.html',
+  styleUrls:['property-list.component.css']
+})
+export class PropertyListComponent implements OnInit
+{
+  properties: any;
+  constructor(private http:HttpClient){}
+  ngOnInit(): void{
+
+  this.http.get('data/properties.json').subscribe(
+    data=>{
+      this.properties=data;
+      console.log(data);
+    }
+  );
+}
+
+}
